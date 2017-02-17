842eaf6f5f6f68537efe29617ca22e841d9e6d3a
Shane <shanebeehler@me.com>
2017-02-17 15:31:53 -0500

    calculate haversine distance from last city and calculate total distance before saving record

--- send_my_love_api/app/serializers/post_serializer.rb
+++ send_my_love_api/app/serializers/post_serializer.rb
@@ -1,3 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :name, :city, :country, :lat, :lng, :created_at
+  attributes :id, :name, :city, :country, :lat, :lng, :distance_from_last_city,
+             :running_total_distance, :created_at
 end
--- send_my_love_api/db/schema.rb
+++ send_my_love_api/db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217160210) do
+ActiveRecord::Schema.define(version: 20170217194156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "city"
     t.string   "country"
     t.float    "lat"
     t.float    "lng"
+    t.float    "distance_from_last_city"
+    t.float    "running_total_distance"
   end
 
 end
